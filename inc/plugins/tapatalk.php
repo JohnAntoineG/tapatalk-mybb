2c8d46f16898afb3ad0b0bb1cdc217a08107f977
jack <jack15083@gmail.com>
2012-11-02 16:15:28 +0800

    fix some small issue

--- inc/plugins/tapatalk.php
+++ inc/plugins/tapatalk.php
@@ -406,7 +406,7 @@ function tt_do_post_request($data,$pushTest = false)
             if(!$fp)
                 return false;
                 
-            $data =  http_build_query($data);
+            $data =  http_build_query($data,'', '&');
             
             fputs($fp, "POST /push.php HTTP/1.1\r\n");
             fputs($fp, "Host: $push_host\r\n");
