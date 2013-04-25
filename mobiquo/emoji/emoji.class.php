1f81313810d7dc06adb8f0a1a966e0185940b147
jack <jack15083@gmail.com>
2013-04-25 10:26:43 +0800

    rmove emoji.log in code

--- mobiquo/emoji/emoji.class.php
+++ mobiquo/emoji/emoji.class.php
@@ -7,16 +7,12 @@ class tapatalkEmoji
     	$data = emoji_kddi_to_unified($data);     # KDDI & Au devices
     	$data = emoji_softbank_to_unified($data); # Softbank & (iPhone) Apple devices
     	$data = emoji_google_to_unified($data);   # Google Android devices
-    	error_log(print_r($data,true),3,"emoji.log");
 		$data = emoji_unified_to_name($data);
 		$data = emoji_unified_to_key($data);
-		//$data = preg_replace('/😀/', "[SMILING FACE WITH OPEN MOUTH]", $data);
-		error_log(print_r($data,true),3,"emoji.log");
 		return $data;
     }
 	static public function covertNameToEmoji($data) {
 		$data = emoji_name_to_unified($data);
-		//$data = emoji_unified_to_google($data);
 		return $data;
 	}
 	static public function covertNameToEmpty($data) {
