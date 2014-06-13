c838f4ab3f9d37baf056b22b3b7676733f3e5c29
jack <jack15083@gmail.com>
2014-06-13 17:44:55 +0800

    update mybb tapatalk to 4.0.0

--- inc/plugins/tapatalk.php
+++ inc/plugins/tapatalk.php
@@ -48,7 +48,7 @@ function tapatalk_info()
         "website"       => "http://tapatalk.com",
         "author"        => "Quoord Systems Limited",
         "authorsite"    => "http://tapatalk.com",
-        "version"       => "3.9.0",
+        "version"       => "4.0.0",
         "guid"          => "e7695283efec9a38b54d8656710bf92e",
         "compatibility" => "16*"
     );
