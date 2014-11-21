f2bdd35cf0d2babe79ac7286d061a3d6fa7a1350
root <root@ip-10-134-161-219.ap-northeast-1.compute.internal>
2014-11-21 12:15:49 +0800

    release mb16 v4.3.1

--- inc/plugins/tapatalk.php
+++ inc/plugins/tapatalk.php
@@ -48,7 +48,7 @@ function tapatalk_info()
         "website"       => "http://tapatalk.com",
         "author"        => "Quoord Systems Limited",
         "authorsite"    => "http://tapatalk.com",
-        "version"       => "4.3.0",
+        "version"       => "4.3.1",
         "guid"          => "e7695283efec9a38b54d8656710bf92e",
         "compatibility" => "1*"
     );
